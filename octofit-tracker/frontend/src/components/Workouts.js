953b9f31561400a964a0da7310f38193044770c3
Matteo Corvaro <72385690+matteocorvaro@users.noreply.github.com>
2025-04-08 13:29:48 +0000

    front

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://humble-space-couscous-p945665gj7726w67-8000.app.github.dev/api/workouts')
+      .then(response => response.json())
+      .then(data => setWorkouts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Workouts</h1>
+      <ul>
+        {workouts.map(workout => (
+          <li key={workout.id}>{workout.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Workouts;
